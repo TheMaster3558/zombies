0d834bf2eed0373e67e047da1f11e4fa4c7e20c6
chawkk6404 <chawk_k@outlook.com>
2022-09-12 16:01:07 -0700

    Add more data

--- setup.py
+++ setup.py
@@ -38,4 +38,20 @@ setuptools.setup(
             "zombies = zombies.cli:main",
         ]
     },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Interpreters",
+        "Typing :: Typed",
+    ],
 )
